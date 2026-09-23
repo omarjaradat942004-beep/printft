d1fc1ae7e35dcbc85116f4f897e2b7d86efb87b3
omajarad <omajarad@c1r1s2.irbid.42.school>
2026-09-23 12:28:59 +0300

    ll

---
+++ ft_printf_str.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_str.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omajarad <omajarad@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/23 12:13:17 by omajarad          #+#    #+#             */
+/*   Updated: 2026/09/23 12:23:12 by omajarad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf.h"
+
+int     ft_printf_str(char *str)
+{
+	size_t	index;
+	size_t  counter;
+
+	index = 0;
+	counter = 0;
+	while (str[index])
+	{
+		write(1,&str[index],1);
+		index++;
+		counter++;
+	}
+	return (counter);
+}
+int main()
+{
+	char str[]="omar jaradat";
+	ft_printf_str(str);
+}
---
+++ printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omajarad <omajarad@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/23 09:56:56 by omajarad          #+#    #+#             */
+/*   Updated: 2026/09/23 10:07:35 by omajarad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+# define PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *ptr, ...);
+
+#endif
